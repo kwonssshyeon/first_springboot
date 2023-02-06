36333d8c9d459b166611c6f1ba0fb4688689a4a2
kwonssshyeon <kwonsh4396@gmail.com>
2023-02-06 16:08:36 +0900

    OAuth2.0 로그인 구현

--- src/main/java/com/ssshyeon/admin/web/IndexController.java
+++ src/main/java/com/ssshyeon/admin/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.ssshyeon.admin.web;
 
+import com.ssshyeon.admin.config.auth.LoginUser;
+import com.ssshyeon.admin.config.auth.dto.SessionUser;
 import com.ssshyeon.admin.domain.posts.PostsRepository;
 import com.ssshyeon.admin.service.posts.PostsService;
 import com.ssshyeon.admin.web.dto.PostsResponseDto;
@@ -9,14 +11,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
@@ -29,7 +38,6 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post",dto);
-
         return "posts-update";
     }
 }
